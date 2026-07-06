6df635288b89cdc99a73266f4632da229d2a5a92
E <emilakbarov857@gmail.com>
2026-07-07 03:22:12 +0400

    Connect and rewrite using response entity

--- src/main/java/com/example/task_manager/controller/TaskController.java
+++ src/main/java/com/example/task_manager/controller/TaskController.java
@@ -1,55 +1,62 @@
 package com.example.task_manager.controller;
 
 import com.example.task_manager.entity.Task;
+import com.example.task_manager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
 
-    private List<Task> tasks = new ArrayList<>();
-    private Long nextId = 1L;
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @GetMapping
     public List<Task> getAllTasks() {
-        return tasks;
+        return taskRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Task getTaskById(@PathVariable Long id) {
-        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        task.setId(nextId++);
-        task.setCreatedAt(LocalDate.from(LocalDateTime.now()));
-        task.setCompleted(false);
-        tasks.add(task);
-        return task;
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task newTask = taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
     }
 
     @PutMapping("/{id}")
-    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
-        for (Task task1 : tasks) {
-            if (task1.getId().equals(id)) {
-                task1.setTitle(task.getTitle());
-                task1.setDescription(task.getDescription());
-                task1.setCompleted(task.getCompleted());
-                task1.setCreatedAt(task.getCreatedAt());
-                return task1;
-            }
-        }
-        return null;
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setCompleted(updatedTask.getCompleted());
+                    Task newTask = taskRepository.save(task);
+                    return ResponseEntity.ok(newTask);
+                }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
-        tasks.removeIf(task -> task.getId().equals(id));
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.ok().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/com/example/task_manager/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.task_manager.repository;
+
+
+import com.example.task_manager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
